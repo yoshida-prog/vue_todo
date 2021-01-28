ac73b2f7effb787439c43226a1030ccc3c641f65
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-29 05:04:07 +0900

    Update index.js
    atom-Beautifyを使用してコードを自動整形

--- src/store/index.js
+++ src/store/index.js
@@ -8,24 +8,24 @@ export default new Vuex.Store({
     todos: []
   },
   mutations: {
-    addTodo(state, comment){
-      if(comment){
-        // mutations内ではgettersが使用できないため直接state.todosにpushした
-        // 以下の2つのmutationについても同じように直接state.todosを使っているが
-        // gettersを使う必要なないのでしょうか？？
-        state.todos.push({Comment: comment, State: '作業中'})
+    addTodo(state, comment) {
+      if (comment) {
+        state.todos.push({
+          Comment: comment,
+          State: '作業中'
+        })
       }
     },
-    deleteTodo(state, e){
+    deleteTodo(state, e) {
       const deleteID = e.target.parentNode.parentNode.firstChild.textContent
       state.todos.splice(deleteID, 1)
     },
-    changeState(state, e){
+    changeState(state, e) {
       const stateName = e.target.textContent
       const changeID = e.target.parentNode.parentNode.firstChild.textContent
-      if(stateName === '作業中'){
+      if (stateName === '作業中') {
         state.todos[changeID]['State'] = '完了'
-      }else if(stateName === '完了'){
+      } else if (stateName === '完了') {
         state.todos[changeID]['State'] = '作業中'
       }
     }
